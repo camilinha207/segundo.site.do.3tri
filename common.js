9892f993351e525ac8e812192711e552513f0778
camilinha207 <c.avelino@escola.pr.gov.br>
2024-09-24 12:12:52 -0300

    ahhhhhh

---
+++ common.js
@@ -0,0 +1,43 @@
+// código omitido
+
+function incluirTexto(texto) {
+
+}
+
+// código omitido
+// código omitido
+
+function incluirTexto(texto) {
+    const container = document.getElementById('graficos-container')
+}
+
+// código omitido
+// código omitido
+
+function incluirTexto(texto) {
+    const container = document.getElementById('graficos-container')
+    const paragrafo = document.createElement('p')
+    paragrafo.classList.add('graficos-container__texto')
+}
+
+// código omitido
+// código omitido
+
+function incluirTexto(texto) {
+    const container = document.getElementById('graficos-container')
+    const paragrafo = document.createElement('p')
+    paragrafo.classList.add('graficos-container__texto')
+    container.appendChild(paragrafo)
+}
+
+// código omitido
+// código omitido
+
+function incluirTexto(texto) {
+    const container = document.getElementById('graficos-container')
+    const paragrafo = document.createElement('p')
+    paragrafo.classList.add('graficos-container__texto')
+    container.appendChild(paragrafo)
+}
+
+export { getCSS, tickConfig, criarGrafico, incluirTexto }
